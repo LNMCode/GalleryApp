ab35152016a93ef34ad3ce6eb201abb116d6415c
LongNguyen31 <longnmp31@gmail.com>
2022-04-24 10:51:15 +0700

    Add lib for project

--- app/src/main/java/com/lnmcode/galleryapp/presentation/MainActivity.kt
+++ app/src/main/java/com/lnmcode/galleryapp/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.lnmcode.galleryapp
+package com.lnmcode.galleryapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lnmcode.galleryapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
