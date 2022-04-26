dc2a746ff43b99cb7a2bce89d4dabb9a390c8672
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 23:07:58 +0700

    Create TopicUserDto

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserDto.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserDto.kt
@@ -2,25 +2,24 @@ package com.lnmcode.galleryapp.business.datasource.network.topic
 
 import com.google.gson.annotations.SerializedName
 
-
 data class TopicUserDto(
-    @SerializedName("id"                 ) val id                : String,
-    @SerializedName("updated_at"         ) val updatedAt         : String,
-    @SerializedName("username"           ) val username          : String,
-    @SerializedName("name"               ) val name              : String,
-    @SerializedName("first_name"         ) val firstName         : String,
-    @SerializedName("last_name"          ) val lastName          : String,
-    @SerializedName("twitter_username"   ) val twitterUsername   : String,
-    @SerializedName("portfolio_url"      ) val portfolioUrl      : String,
-    @SerializedName("bio"                ) val bio               : String,
-    @SerializedName("location"           ) val location          : String,
-    @SerializedName("links"              ) val topicUserLinksDto : TopicUserLinksDto ,
-    @SerializedName("profile_image"      ) val topicUserprofileImageDto : TopicUserProfileImageDto,
-    @SerializedName("instagram_username" ) val instagramUsername : String,
-    @SerializedName("total_collections"  ) val totalCollections  : Int,
-    @SerializedName("total_likes"        ) val totalLikes        : Int,
-    @SerializedName("total_photos"       ) val totalPhotos       : Int,
-    @SerializedName("accepted_tos"       ) val acceptedTos       : Boolean,
-    @SerializedName("for_hire"           ) val forHire           : Boolean,
+    @SerializedName("id"                 ) var id                : String,
+    @SerializedName("updated_at"         ) var updatedAt         : String,
+    @SerializedName("username"           ) var username          : String,
+    @SerializedName("name"               ) var name              : String,
+    @SerializedName("first_name"         ) var firstName         : String,
+    @SerializedName("last_name"          ) var lastName          : String,
+    @SerializedName("twitter_username"   ) var twitterUsername   : String,
+    @SerializedName("portfolio_url"      ) var portfolioUrl      : String,
+    @SerializedName("bio"                ) var bio               : String,
+    @SerializedName("location"           ) var location          : String,
+    @SerializedName("links"              ) var topicUserLinksDto: TopicUserLinksDto  ,
+    @SerializedName("profile_image"      ) var topicUserProfileImageDto: TopicUserProfileImageDto,
+    @SerializedName("instagram_username" ) var instagramUsername : String,
+    @SerializedName("total_collections"  ) var totalCollections  : Int,
+    @SerializedName("total_likes"        ) var totalLikes        : Int,
+    @SerializedName("total_photos"       ) var totalPhotos       : Int,
+    @SerializedName("accepted_tos"       ) var acceptedTos       : Boolean,
+    @SerializedName("for_hire"           ) var forHire           : Boolean,
 
 )
