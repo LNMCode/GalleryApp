562df627f885a1e5a31305e111dbec2868a8c9e8
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 23:03:49 +0700

    Create TopicLinkDto

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicLinksDto.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicLinksDto.kt
@@ -3,8 +3,7 @@ package com.lnmcode.galleryapp.business.datasource.network.topic
 import com.google.gson.annotations.SerializedName
 
 data class TopicLinksDto (
-    @SerializedName("self"              ) val self             : String,
-    @SerializedName("html"              ) val html             : String,
-    @SerializedName("download"          ) val download         : String,
-    @SerializedName("download_location" ) val downloadLocation : String
-)
+    @SerializedName("self"   ) val self   : String,
+    @SerializedName("html"   ) val html   : String,
+    @SerializedName("photos" ) val photos : String
+    )
