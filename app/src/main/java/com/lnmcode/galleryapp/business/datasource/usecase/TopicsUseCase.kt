88bf5ca3b99581831ae741e7841c6b75f7dcb841
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 21:20:21 +0700

    Create TopicUseCase

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicsUseCase.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicsUseCase.kt
@@ -1,4 +1,11 @@
 package com.lnmcode.galleryapp.business.datasource.usecase
 
-class TopicsUseCase {
+import com.lnmcode.galleryapp.business.datasource.network.helper.topics.TopicsIApiRepository
+import com.lnmcode.galleryapp.business.datasource.network.topics.respose.Topics
+
+class TopicsUseCase(private val topicsIApiRepository: TopicsIApiRepository) {
+    suspend fun getTopics(key: String): Topics {
+        return topicsIApiRepository.topics(key = key)
+    }
+
 }
