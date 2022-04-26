5f442d5e09378a89af17ab299cd64f8a95a51889
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 21:06:29 +0700

    CreateUrlsDto

---
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUrlsDto.kt
@@ -0,0 +1,12 @@
+package com.lnmcode.galleryapp.business.datasource.network.topic
+
+import com.google.gson.annotations.SerializedName
+
+data class TopicUrlsDto(
+    @SerializedName("raw"      ) val raw     : String,
+    @SerializedName("full"     ) val full    : String,
+    @SerializedName("regular"  ) val regular : String,
+    @SerializedName("small"    ) val small   : String,
+    @SerializedName("thumb"    ) val thumb   : String,
+    @SerializedName("small_s3" ) val smallS3 : String
+)
