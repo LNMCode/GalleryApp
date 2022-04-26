487b84864f63c1426f5b024696c6062a2e05030b
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 23:08:07 +0700

    Create TopicUserLinksDto

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserLinksDto.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserLinksDto.kt
@@ -6,8 +6,10 @@ data class TopicUserLinksDto (
     @SerializedName("self"      ) val self      : String,
     @SerializedName("html"      ) val html      : String,
     @SerializedName("photos"    ) val photos    : String,
-    @SerializedName("likes"     ) val likes     : String?,
+    @SerializedName("likes"     ) val likes     : String,
     @SerializedName("portfolio" ) val portfolio : String,
     @SerializedName("following" ) val following : String,
     @SerializedName("followers" ) val followers : String
- )
+        )
+
+
