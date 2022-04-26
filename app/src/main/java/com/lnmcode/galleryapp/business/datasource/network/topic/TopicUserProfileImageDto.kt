d2b8df526dc3ce137d7e3f849c4a400db2c4442f
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-26 23:08:17 +0700

    Create TopicUserProfileImageDto

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserProfileImageDto.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicUserProfileImageDto.kt
@@ -2,7 +2,7 @@ package com.lnmcode.galleryapp.business.datasource.network.topic
 
 import com.google.gson.annotations.SerializedName
 
-data class TopicUserProfileImageDto(
+data class TopicUserProfileImageDto (
     @SerializedName("small"  ) val small  : String,
     @SerializedName("medium" ) val medium : String,
     @SerializedName("large"  ) val large  : String
