53c86e0ef1d25d009de9fc74a145a20ef1bbf869
LongNguyen31 <longnmp31@gmail.com>
2022-04-27 09:30:31 +0700

    Add single for network and repo module

--- app/src/main/java/com/lnmcode/galleryapp/di/NetworkModule.kt
+++ app/src/main/java/com/lnmcode/galleryapp/di/NetworkModule.kt
@@ -1,20 +1,36 @@
 package com.lnmcode.galleryapp.di
 
 import com.lnmcode.galleryapp.business.datasource.interceptor.RequestInterceptor
+import com.lnmcode.galleryapp.business.datasource.network.helper.search.SearchApiService
+import com.lnmcode.galleryapp.business.datasource.network.helper.topic.TopicApiService
+import com.lnmcode.galleryapp.business.datasource.network.helper.topicphoto.TopicPhotoApiService
+import com.lnmcode.galleryapp.business.datasource.network.helper.topics.TopicsApiService
+import com.lnmcode.galleryapp.business.domain.utils.Constants
 import okhttp3.OkHttpClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
 
+    single(named(Constants.BASE_URL_API_NAME)) { baseUrlString() }
+
     single { RequestInterceptor() }
 
     single { okHttpClientBuilder(get()) }
 
-    single { retrofitBuilder(get()) }
+    single { retrofitBuilder(get(), get(qualifier = named(Constants.BASE_URL_API_NAME))) }
 
-    //single { get<Retrofit>().create() }
+    // Search service
+    single { get<Retrofit>().create(SearchApiService::class.java) }
+
+    // Topic service
+    single { get<Retrofit>().create(TopicApiService::class.java) }
+
+    single { get<Retrofit>().create(TopicPhotoApiService::class.java) }
+
+    single { get<Retrofit>().create(TopicsApiService::class.java) }
 
 }
 
@@ -27,11 +43,16 @@ internal fun okHttpClientBuilder(
 }
 
 internal fun retrofitBuilder(
-    okHttpClient: OkHttpClient
+    okHttpClient: OkHttpClient,
+    baseUrlAPI: String,
 ): Retrofit {
     return Retrofit.Builder()
         .client(okHttpClient)
-        .baseUrl("")
+        .baseUrl(baseUrlAPI)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+}
+
+internal fun baseUrlString(): String {
+    return "https://api.unsplash.com/"
 }
