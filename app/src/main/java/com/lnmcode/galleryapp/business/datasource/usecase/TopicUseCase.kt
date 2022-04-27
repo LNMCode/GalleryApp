786d4680064a5a518e55f65716e00f571c95b2eb
LongNguyen31 <longnmp31@gmail.com>
2022-04-27 08:17:41 +0700

    Fix issue name class

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicUseCase.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicUseCase.kt
@@ -1,8 +1,9 @@
 package com.lnmcode.galleryapp.business.datasource.usecase
 import com.lnmcode.galleryapp.business.datasource.network.helper.topic.TopicApiRepository
-import com.lnmcode.galleryapp.business.datasource.network.topic.reponse.Topic
+import com.lnmcode.galleryapp.business.datasource.network.topics.respose.Topics
+
 class TopicUseCase(private val topicApiRepository: TopicApiRepository) {
-    suspend fun getTopics(id :String ,key: String): Topic {
+    suspend fun getTopics(id :String ,key: String): Topics {
         return topicApiRepository.topic(id =id ,key = key)
     }
 
