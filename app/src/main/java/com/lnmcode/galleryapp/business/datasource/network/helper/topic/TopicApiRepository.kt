44de5bf61e832e996fa4d27897eb204d30b77ca7
LongNguyen31 <longnmp31@gmail.com>
2022-04-27 08:04:47 +0700

    Fix issue

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/helper/topic/TopicApiRepository.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/helper/topic/TopicApiRepository.kt
@@ -1,11 +1,10 @@
 package com.lnmcode.galleryapp.business.datasource.network.helper.topic
 
-import com.lnmcode.galleryapp.business.datasource.network.topic.reponse.Topic
-import com.lnmcode.galleryapp.business.datasource.network.topicphoto.reponse.TopicPhoto
+import com.lnmcode.galleryapp.business.datasource.network.topics.respose.Topics
 
 interface TopicApiRepository {
     suspend fun topic(
         id :String,
         key :String
-    ) : Topic
+    ) : Topics
 }
