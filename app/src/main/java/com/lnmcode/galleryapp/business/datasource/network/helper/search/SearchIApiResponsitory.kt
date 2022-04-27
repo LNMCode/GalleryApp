063d58e23f69f2b41f4b84945cdc659bce461474
LongNguyen31 <longnmp31@gmail.com>
2022-04-27 09:29:06 +0700

    Rename for search repository

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/helper/search/SearchIApiResponsitory.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/helper/search/SearchIApiResponsitory.kt
@@ -2,7 +2,7 @@ package com.lnmcode.galleryapp.business.datasource.network.helper.search
 
 import com.lnmcode.galleryapp.business.datasource.network.search.response.Search
 
-interface SearchIApiRepository {
+interface SearchApiRepository {
     suspend fun search(
         key :String,
         query :String
