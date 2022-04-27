9704864efe73aaaa4a1431e2fa40cca4370c6468
LongNguyen31 <longnmp31@gmail.com>
2022-04-27 09:36:32 +0700

    Add single for usecase module

--- app/src/main/java/com/lnmcode/galleryapp/di/UseCaseModule.kt
+++ app/src/main/java/com/lnmcode/galleryapp/di/UseCaseModule.kt
@@ -1,7 +1,18 @@
 package com.lnmcode.galleryapp.di
 
+import com.lnmcode.galleryapp.business.datasource.usecase.SearchUseCase
+import com.lnmcode.galleryapp.business.datasource.usecase.TopicPhotoUseCase
+import com.lnmcode.galleryapp.business.datasource.usecase.TopicUseCase
+import com.lnmcode.galleryapp.business.datasource.usecase.TopicsUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
 
+    single { SearchUseCase(get()) }
+
+    single { TopicPhotoUseCase(get()) }
+
+    single { TopicsUseCase(get()) }
+
+    single { TopicUseCase(get()) }
 }
