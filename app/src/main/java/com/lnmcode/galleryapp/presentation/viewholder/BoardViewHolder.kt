b407afd42821e9ce0429e1ff0a1bcb8252cb46a6
LongNguyen31 <longnmp31@gmail.com>
2022-04-29 15:50:51 +0700

    Add event insert for board card

--- app/src/main/java/com/lnmcode/galleryapp/presentation/viewholder/BoardViewHolder.kt
+++ app/src/main/java/com/lnmcode/galleryapp/presentation/viewholder/BoardViewHolder.kt
@@ -2,12 +2,17 @@ package com.lnmcode.galleryapp.presentation.viewholder
 
 import android.view.View
 import androidx.core.view.ViewCompat
+import com.lnmcode.galleryapp.business.datasource.cache.topics.TopicsEntities
+import com.lnmcode.galleryapp.business.domain.cache.TopicsCacheDomain
 import com.lnmcode.galleryapp.business.domain.models.topics.Topics
 import com.lnmcode.galleryapp.databinding.LayoutItemBoardsBinding
 import com.lnmcode.galleryapp.presentation.adapter.BaseViewHolder
 import timber.log.Timber
 
-class BoardViewHolder(view: View): BaseViewHolder(view) {
+class BoardViewHolder(
+    view: View,
+    private val insertTopicsCache: (TopicsCacheDomain) -> Unit,
+) : BaseViewHolder(view) {
 
     private lateinit var data: Topics
     private val binding: LayoutItemBoardsBinding by bindings()
@@ -22,6 +27,15 @@ class BoardViewHolder(view: View): BaseViewHolder(view) {
     private fun setUpLayout() {
         binding.apply {
             topic = data
+            imgIconCard.setOnClickListener {
+                val topicsData = TopicsCacheDomain(
+                    id = data.id,
+                    slug = data.slug,
+                    title = data.title,
+                    description = data.description
+                )
+                insertTopicsCache(topicsData)
+            }
         }
     }
 
