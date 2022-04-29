5a93ef1f494d62d90215b5b5674c7b734ec16e00
LongNguyen31 <longnmp31@gmail.com>
2022-04-29 15:50:28 +0700

    Create binding and event for board adapter

--- app/src/main/java/com/lnmcode/galleryapp/presentation/ui/boards/BoardsAdapter.kt
+++ app/src/main/java/com/lnmcode/galleryapp/presentation/ui/boards/BoardsAdapter.kt
@@ -2,12 +2,17 @@ package com.lnmcode.galleryapp.presentation.ui.boards
 
 import android.view.View
 import com.lnmcode.galleryapp.R
+import com.lnmcode.galleryapp.business.datasource.cache.topics.TopicsEntities
+import com.lnmcode.galleryapp.business.datasource.usecase.TopicsCacheUseCase
+import com.lnmcode.galleryapp.business.domain.cache.TopicsCacheDomain
 import com.lnmcode.galleryapp.business.domain.models.topics.Topics
 import com.lnmcode.galleryapp.presentation.adapter.BaseAdapter
 import com.lnmcode.galleryapp.presentation.adapter.SectionRow
 import com.lnmcode.galleryapp.presentation.viewholder.BoardViewHolder
 
-class BoardsAdapter : BaseAdapter(){
+class BoardsAdapter(
+    private val insertTopicsCache: (topicsCacheDomain: TopicsCacheDomain) -> Unit,
+) : BaseAdapter() {
 
     init {
         addSection(arrayListOf<Topics>())
@@ -23,5 +28,8 @@ class BoardsAdapter : BaseAdapter(){
 
     override fun layout(sectionRow: SectionRow) = R.layout.layout_item_boards
 
-    override fun viewHolder(layout: Int, view: View) = BoardViewHolder(view)
+    override fun viewHolder(layout: Int, view: View) =
+        BoardViewHolder(view) {
+            insertTopicsCache(it)
+        }
 }
--- app/src/main/java/com/lnmcode/galleryapp/presentation/ui/boards/BoardsViewModel.kt
+++ app/src/main/java/com/lnmcode/galleryapp/presentation/ui/boards/BoardsViewModel.kt
@@ -8,9 +8,12 @@ import com.lnmcode.galleryapp.bindables.bindingProperty
 import com.lnmcode.galleryapp.business.datasource.usecase.TopicsCacheUseCase
 import com.lnmcode.galleryapp.business.domain.models.topics.Topics
 import com.lnmcode.galleryapp.business.datasource.usecase.TopicsUseCase
+import com.lnmcode.galleryapp.business.domain.cache.TopicsCacheDomain
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import timber.log.Timber
 
-class BoardsViewModel (
+class BoardsViewModel(
     private val topicsUseCase: TopicsUseCase,
     private val topicsCacheUseCase: TopicsCacheUseCase,
 ) : BindingViewModel() {
@@ -25,6 +28,45 @@ class BoardsViewModel (
 
     @get:Bindable
     val topics: List<Topics> by topicsFlow.asBindingProperty(viewModelScope, emptyList())
+
+    private val topicsCacheStateFlow = MutableStateFlow<Long>(-1)
+    private val topicsCacheFlow = topicsCacheStateFlow.flatMapLatest {
+        topicsCacheUseCase.getTopics(
+            onSuccess = {
+                isLoading = true
+            }
+        )
+    }
+
+    @get:Bindable
+    val topicsCache: List<TopicsCacheDomain> by topicsCacheFlow.asBindingProperty(
+        viewModelScope,
+        emptyList()
+    )
+
+    private val topicsDomainStateFlow = MutableStateFlow<TopicsCacheDomain?>(null)
+    private val topicsDomainFlow = topicsDomainStateFlow.flatMapLatest {
+        topicsCacheUseCase.insertAndReplace(
+            it,
+            onSuccess = {
+                onRefreshTopicsFromCache()
+                isLoading = true
+            }
+        )
+    }
+
+    @get:Bindable
+    val topicsDomainLong: Long? by topicsDomainFlow.asBindingProperty(viewModelScope, -1)
+
+    fun insertTopicsCache(topicsCacheDomain: TopicsCacheDomain) {
+        Timber.d(topicsCacheDomain.toString())
+        topicsDomainStateFlow.value = topicsCacheDomain
+    }
+
+    private fun onRefreshTopicsFromCache() {
+        Timber.d("onRefreshTopicsFromCache $topicsDomainLong")
+        topicsCacheStateFlow.value = topicsDomainLong ?: 0
+    }
 
     init {
         Timber.d("Init Boards View model")
