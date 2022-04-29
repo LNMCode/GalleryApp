30d8f10ce2854372ce46c3c06bd99a96b394b1b5
LongNguyen31 <longnmp31@gmail.com>
2022-04-29 15:49:59 +0700

    Add mapper for topics cache domain

--- app/src/main/java/com/lnmcode/galleryapp/business/domain/cache/TopicsCacheDomain.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/domain/cache/TopicsCacheDomain.kt
@@ -1,8 +1,16 @@
 package com.lnmcode.galleryapp.business.domain.cache
 
+import com.lnmcode.galleryapp.business.datasource.cache.topics.TopicsEntities
+
 data class TopicsCacheDomain(
     val id: String,
     val slug: String,
     val title: String,
     val description: String,
 )
+
+fun TopicsCacheDomain.toEntity(): TopicsEntities {
+    return TopicsEntities(
+        id, slug, title, description
+    )
+}
