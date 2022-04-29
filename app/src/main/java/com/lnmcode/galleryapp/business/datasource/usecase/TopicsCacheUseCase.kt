35b125a775c9695442b2dcf06779fc9afca33021
LongNguyen31 <longnmp31@gmail.com>
2022-04-29 15:48:28 +0700

    Update topics cache usecase to workerthread

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicsCacheUseCase.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/usecase/TopicsCacheUseCase.kt
@@ -1,22 +1,52 @@
 package com.lnmcode.galleryapp.business.datasource.usecase
 
+import androidx.annotation.WorkerThread
 import com.lnmcode.galleryapp.business.datasource.cache.helper.TopicsCacheRepository
-import com.lnmcode.galleryapp.business.datasource.cache.topics.TopicsEntities
+import com.lnmcode.galleryapp.business.datasource.cache.topics.toTopicsCacheDomain
+import com.lnmcode.galleryapp.business.domain.cache.TopicsCacheDomain
+import com.lnmcode.galleryapp.business.domain.cache.toEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import timber.log.Timber
 
 class TopicsCacheUseCase(
     private val topicsCacheRepository: TopicsCacheRepository,
 ) {
-    suspend fun getTopics(): List<TopicsEntities> {
-        return topicsCacheRepository.getTopics()
+
+    init {
+        Timber.d("Injection TopicsCacheUseCase")
     }
 
-    suspend fun getTopicsFromId(id: String): TopicsEntities {
-        return topicsCacheRepository.getTopicsFromId(id)
+    @WorkerThread
+    fun getTopics(
+        onSuccess: () -> Unit,
+    ): Flow<List<TopicsCacheDomain>> = flow {
+        val topicsCache = topicsCacheRepository.getTopics().map { it.toTopicsCacheDomain() }
+        emit(topicsCache)
+    }.onCompletion { onSuccess() }.flowOn(Dispatchers.IO).catch { e ->
+        Timber.e(e.message)
     }
 
-    suspend fun insertAndReplace(
-        topicsEntities: TopicsEntities
-    ): Long {
-        return topicsCacheRepository.insertAndReplace(topicsEntities)
+    @WorkerThread
+    fun getTopicsFromId(
+        id: String,
+        onSuccess: () -> Unit,
+    ): Flow<TopicsCacheDomain> = flow {
+        val topics = topicsCacheRepository.getTopicsFromId(id).toTopicsCacheDomain()
+        emit(topics)
+    }.onCompletion { onSuccess() }.flowOn(Dispatchers.IO).catch { e ->
+        Timber.e(e.message)
+    }
+
+    @WorkerThread
+    fun insertAndReplace(
+        topicsCacheDomain: TopicsCacheDomain?,
+        onSuccess: () -> Unit,
+    ) = flow {
+        val topicsEntities = topicsCacheDomain?.toEntity()
+        val longInsert = topicsEntities?.let { topicsCacheRepository.insertAndReplace(it) }
+        emit(longInsert)
+    }.onCompletion { onSuccess() }.flowOn(Dispatchers.IO).catch { e ->
+        Timber.e(e.message)
     }
 }
