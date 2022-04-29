30e3009190b107a00166e1cc7648514e8339ea02
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-29 12:32:37 +0700

    create TopicTopContributorsLink

---
+++ app/src/main/java/com/lnmcode/galleryapp/business/domain/models/topic/TopicTopContributorsLinks.kt
@@ -0,0 +1,13 @@
+package com.lnmcode.galleryapp.business.domain.models.topic
+
+import com.google.gson.annotations.SerializedName
+
+data class TopicTopContributorsLinks(
+    val self      : String?,
+    val html      : String?,
+    val photos    : String?,
+    val likes     : String?,
+    val portfolio : String?,
+    val following : String?,
+    val followers : String?
+)
