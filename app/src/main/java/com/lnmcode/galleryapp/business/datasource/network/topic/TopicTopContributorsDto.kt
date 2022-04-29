0e9500143d835c4373692c8789e033a9b154b8eb
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-29 12:29:53 +0700

    Create add fun toTopicTopContributors

--- app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicTopContributorsDto.kt
+++ app/src/main/java/com/lnmcode/galleryapp/business/datasource/network/topic/TopicTopContributorsDto.kt
@@ -1,25 +1,51 @@
 package com.lnmcode.galleryapp.business.datasource.network.topic
 
 import com.google.gson.annotations.SerializedName
+import com.lnmcode.galleryapp.business.domain.models.topic.TopicTopContributors
+import com.lnmcode.galleryapp.business.domain.models.topic.TopicTopContributorsImageProfile
+import com.lnmcode.galleryapp.business.domain.models.topic.TopicTopContributorsLinks
 
 data class TopicTopContributorsDto(
-    @SerializedName("id"                 ) val id                : String?       = null,
-    @SerializedName("updated_at"         ) val updatedAt         : String?       = null,
-    @SerializedName("username"           ) val username          : String?       = null,
-    @SerializedName("name"               ) val name              : String?       = null,
-    @SerializedName("first_name"         ) val firstName         : String?       = null,
-    @SerializedName("last_name"          ) val lastName          : String?       = null,
-    @SerializedName("twitter_username"   ) val twitterUsername   : String?       = null,
-    @SerializedName("portfolio_url"      ) val portfolioUrl      : String?       = null,
-    @SerializedName("bio"                ) val bio               : String?       = null,
-    @SerializedName("location"           ) val location          : String?       = null,
-    @SerializedName("links"              ) val topicTopContributorsLinksDto: TopicTopContributorsLinksDto,
-    @SerializedName("profile_image"      ) val  topicTopContributorsImageProfileDto: TopicTopContributorsImageProfileDto,
-    @SerializedName("instagram_username" ) val instagramUsername : String?       = null,
-    @SerializedName("total_collections"  ) val totalCollections  : Int?          = null,
-    @SerializedName("total_likes"        ) val totalLikes        : Int?          = null,
-    @SerializedName("total_photos"       ) val totalPhotos       : Int?          = null,
-    @SerializedName("accepted_tos"       ) val acceptedTos       : Boolean?      = null,
-    @SerializedName("for_hire"           ) val forHire           : Boolean?      = null,
+    @SerializedName("id"                 ) val id                : String?,
+    @SerializedName("updated_at"         ) val updatedAt         : String?,
+    @SerializedName("username"           ) val username          : String?,
+    @SerializedName("name"               ) val name              : String?,
+    @SerializedName("first_name"         ) val firstName         : String?,
+    @SerializedName("last_name"          ) val lastName          : String?,
+    @SerializedName("twitter_username"   ) val twitterUsername   : String?,
+    @SerializedName("portfolio_url"      ) val portfolioUrl      : String?,
+    @SerializedName("bio"                ) val bio               : String?,
+    @SerializedName("location"           ) val location          : String?,
+    @SerializedName("links"              ) val topicTopContributorsLinksDto: TopicTopContributorsLinksDto?,
+    @SerializedName("profile_image"      ) val  topicTopContributorsImageProfileDto: TopicTopContributorsImageProfileDto?,
+    @SerializedName("instagram_username" ) val instagramUsername : String?,
+    @SerializedName("total_collections"  ) val totalCollections  : Int?,
+    @SerializedName("total_likes"        ) val totalLikes        : Int?,
+    @SerializedName("total_photos"       ) val totalPhotos       : Int?,
+    @SerializedName("accepted_tos"       ) val acceptedTos       : Boolean?,
+    @SerializedName("for_hire"           ) val forHire           : Boolean?,
 
-)
+    )
+fun TopicTopContributorsDto.toTopicTopContributors() :TopicTopContributors{
+    return TopicTopContributors(
+        id =id,
+        updatedAt =updatedAt,
+        username=username,
+        name =name,
+        firstName= firstName,
+        lastName = lastName,
+        twitterUsername =twitterUsername,
+        portfolioUrl=portfolioUrl,
+        bio=bio,
+        location=location,
+        topicTopContributorsImageProfile = topicTopContributorsImageProfileDto?.toTopicTopContributorsImageProfile(),
+        topicTopContributorsLinks = topicTopContributorsLinksDto?.toTopicTopContributorsLinks(),
+        instagramUsername = instagramUsername,
+        totalCollections =totalCollections,
+        totalLikes = totalLikes,
+        totalPhotos = totalPhotos,acceptedTos = acceptedTos,
+        forHire = forHire
+
+
+    )
+}
