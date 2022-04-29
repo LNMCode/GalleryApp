6dab88b8fd7941f5d054fc85f24a00e83762933f
trandinhhung <tdhung.18it4@vku.udn.vn>
2022-04-29 12:32:10 +0700

    create TopicOwnersProfileImage

---
+++ app/src/main/java/com/lnmcode/galleryapp/business/domain/models/topic/TopicOwnersProfileImage.kt
@@ -0,0 +1,9 @@
+package com.lnmcode.galleryapp.business.domain.models.topic
+
+import com.google.gson.annotations.SerializedName
+
+data class TopicOwnersProfileImage(
+   val small  : String?,
+   val medium : String?,
+   val large  : String?
+)
