56ee644071358e7a1aa315866a0d26827ef5dcdb
LNMCode <longnmp31@gmail.com>
2022-04-30 19:44:05 +0700

    Inti layout detail

--- app/src/main/java/com/lnmcode/galleryapp/presentation/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/lnmcode/galleryapp/presentation/ui/detail/DetailFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.lnmcode.galleryapp.R
 import com.lnmcode.galleryapp.bindables.BindingFragment
 import com.lnmcode.galleryapp.databinding.FragmentDetailBinding
+import timber.log.Timber
 
 class DetailFragment : BindingFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,29 @@ class DetailFragment : BindingFragment<FragmentDetailBinding>(R.layout.fragment_
         return binding {
 
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding {
+            bottomSheetBehavior = getBottomSheetBehavior(bottomSheet)
+            showBottomSheet.setOnClickListener {
+                showAndHideBottomSheet()
+            }
+        }
+    }
+
+    private fun getBottomSheetBehavior(bottomSheet: View): BottomSheetBehavior<View> {
+        return BottomSheetBehavior.from(bottomSheet).apply {
+            state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun showAndHideBottomSheet() {
+        Timber.d("${bottomSheetBehavior.state}")
+        if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        else bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
 }
